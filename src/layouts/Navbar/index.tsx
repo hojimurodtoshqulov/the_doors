fd83e978fbe95fe1daa9a2cbf8db0f77cff7240b
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-04-06 17:31:11 +0500

    navbar started

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import logo from "../../../public/media/logo.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { BiSearch } from "react-icons/bi";
+import styles from "./navbar.module.scss";
+import Link from "next/link";
+
+type MenuRouteType = {
+  link: string;
+  label: string;
+};
+
+const menuRoutes: MenuRouteType[] = [
+  {
+    link: "/",
+    label: "Home",
+  },
+  {
+    link: "/",
+    label: "Home",
+  },
+  {
+    link: "/",
+    label: "About",
+  },
+  {
+    link: "/",
+    label: "Blog",
+  },
+  {
+    link: "/",
+    label: "Contact",
+  },
+];
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className={`${styles.navbar} container-padding`}>
+      <Link href="/">
+        <a className="logo">
+          <img src={logo.src} />
+        </a>
+      </Link>
+      <div className={styles.menu}>
+        {menuRoutes.map((route: MenuRouteType) => (
+          <Link href={route.link}>
+            <a className={"link"}>{route.label}</a>
+          </Link>
+        ))}
+      </div>
+      <div className="icons">
+        <BiSearch />
+        <BsPerson />
+        <AiOutlineShoppingCart />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
