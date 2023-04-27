1a805f9c0a919c17cb654ee97ec3123ee1310827
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-04-27 16:08:43 +0500

    video updated

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -4,28 +4,29 @@ import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 
 function Video({ src }: { src: string }) {
-  const [play, setPlay] = useState(true);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    if (!videoRef) return;
+	const [play, setPlay] = useState(true);
+	const videoRef = useRef<HTMLVideoElement>(null);
+	useEffect(() => {
+		if (!videoRef) return;
 
-    play ? videoRef?.current?.play() : videoRef?.current?.pause();
-  }, [play]);
+		play ? videoRef?.current?.play() : videoRef?.current?.pause();
+	}, [play]);
 
-  return (
-    <div className={styles.video} onClick={() => setPlay(!play)}>
-      <video autoPlay loop muted ref={videoRef}>
-        <source src={src} type="video/mp4" />{" "}
-      </video>{" "}
-      {play ? (
-        ""
-      ) : (
-        <>
-          <AiFillPlayCircle size={50} /> <Link href="/">Request a call</Link>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className={styles.video} onClick={() => setPlay(!play)}>
+			{/* <video >
+        <source   />
+      </video> */}
+			<video src={src} autoPlay loop muted ref={videoRef} />
+			{play ? (
+				""
+			) : (
+				<>
+					<AiFillPlayCircle size={50} /> <Link href="/">Request a call</Link>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default Video;
--- src/components/sections/VideoSection/index.tsx
+++ src/components/sections/VideoSection/index.tsx
@@ -3,40 +3,41 @@ import { useState, useEffect, useRef } from "react";
 import useIntersectionObserver from "@/utils/InterSectionObserver";
 
 function VideoSection() {
-  const [scrollPosition, setScrollPosition] = useState<number>(0);
-  const vdRef = useRef<HTMLVideoElement>(null);
-  const ref = useRef(null);
-  const entity = useIntersectionObserver(ref, {});
+	const [scrollPosition, setScrollPosition] = useState<number>(0);
+	const vdRef = useRef<HTMLVideoElement>(null);
+	const ref = useRef(null);
+	const entity = useIntersectionObserver(ref, {});
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scroll = window.pageYOffset;
-      setScrollPosition(scroll);
-      if (!vdRef.current) {
-        return;
-      }
-      if (scroll - 20 < 0) {
-        vdRef.current.currentTime = 0;
-      }
-      window.pageYOffset < 3000
-        ? vdRef.current?.play()
-        : vdRef.current?.pause();
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-  return (
-    <div
-      className={styles.video}
-      style={{ clipPath: `circle(${scrollPosition / 10}% at 65% 55%)` }}
-      ref={ref}
-    >
-      {/* <video src="public/media/site.mp4" controls></video> */}
-      <video loop muted ref={vdRef}>
+	useEffect(() => {
+		const handleScroll = () => {
+			const scroll = window.pageYOffset;
+			setScrollPosition(scroll);
+			if (!vdRef.current) {
+				return;
+			}
+			if (scroll - 20 < 0) {
+				vdRef.current.currentTime = 0;
+			}
+			window.pageYOffset < 3000
+				? vdRef.current?.play()
+				: vdRef.current?.pause();
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+	return (
+		<div
+			className={styles.video}
+			style={{ clipPath: `circle(${scrollPosition / 10}% at 65% 55%)` }}
+			ref={ref}
+		>
+			{/* <video src="public/media/site.mp4" controls></video> */}
+			{/* <video loop muted ref={vdRef}>
         <source src="/media/site.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
+      </video> */}
+			<video src="/media/site.mp4" loop muted ref={vdRef} />
+		</div>
+	);
 }
 
 export default VideoSection;
