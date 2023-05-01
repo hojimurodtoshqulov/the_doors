e245caabf35d38cfab118e0f2bb24b59ceaea94e
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-05-01 14:42:07 +0500

    bug fixed

--- src/components/ModalImages/index.tsx
+++ src/components/ModalImages/index.tsx
@@ -1,21 +1,36 @@
 import { MouseEvent, useEffect, useMemo, useRef, useState } from "react";
 import styles from "./styles.module.scss";
+import hero from "../../../public/media/Rectangle 6959.png";
 import ImageMagnifierGlass from "../ImageMagnifier";
 
 function ModalImages({ images }: { images?: { data: string }[] }) {
+  const [image, setImage] = useState<string>(images?.[0].data || "");
+
+  useEffect(() => {
+    setImage(images?.[0].data || "");
+  }, [images]);
+
+  console.log(images);
+
   return (
     <div className={styles.images}>
       <div className={styles.miniimages}>
-        {images?.slice(1, 4).map((image: { data: string }) => (
+        {images?.map((image: { data: string }) => (
           <img
             src={`data:image/png;base64,${image.data}`}
             onClick={(e) => {
-              e.currentTarget.classList.toggle(styles.active);
+              setImage(image.data);
             }}
           />
         ))}
+        {/* <img
+          src={`data:image/png;base64,${images?.[0].data}`}
+          onClick={(e) => {
+            setImage(images?.[0].data || "");
+          }}
+        /> */}
       </div>
-      <ImageMagnifierGlass imageSrc={images?.[0].data || ""} />
+      <ImageMagnifierGlass imageSrc={image} />
     </div>
   );
 }
