ee23692dff76330fc4f82ff57e15d49accfc7d4c
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-05-02 23:17:43 +0500

    bug fixed

--- src/components/ProductModal/index.tsx
+++ src/components/ProductModal/index.tsx
@@ -48,6 +48,12 @@ function ProductModal({
           progress: undefined,
           theme: "light",
         });
+        setForm({
+          fullName: "",
+          phoneNumber: "",
+          quantity: 1,
+          productId: product.id,
+        });
       })
       .catch(() =>
         toast.error("Cannot send your order", {
