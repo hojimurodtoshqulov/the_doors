e71ec86bf46b2ebb5221c83efdc33df74a084127
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-05-10 13:17:43 +0500

    shit fixed

--- src/components/sections/Accordion/index.tsx
+++ src/components/sections/Accordion/index.tsx
@@ -3,7 +3,12 @@ import img from "../../../../public/media/CONTACTHERO.png";
 import { Collapse } from "antd";
 import Title from "@/components/Title";
 import scss from "./accordion.module.scss";
-import { FaqDataItem, StepItem, faqData, faqtInstallationData } from "@/data/faq.data";
+import {
+  FaqDataItem,
+  StepItem,
+  faqData,
+  faqtInstallationData,
+} from "@/data/faq.data";
 const { Panel } = Collapse;
 
 const text = `
@@ -17,23 +22,21 @@ const Accordion: React.FC = () => (
     <Title style={{ color: "#666" }}>ОСОБЕННОСТИ И ПРЕИМУЩЕСТВА</Title>
     <Collapse accordion className={scss.accordion}>
       {faqData.map((faq: FaqDataItem, i) => (
-        <Panel header={faq.quiz} key={i}>
+        <Panel header={<h1>{faq.quiz}</h1>} key={i}>
           <div className={scss.body}>
             <p>{faq.answer}</p>
           </div>
         </Panel>
       ))}
-      <Panel header={faqtInstallationData.quiz} key="-1">
+      <Panel header={<h1>{faqtInstallationData.quiz}</h1>} key="-1">
         <div className={scss.body}>
-		{
-			faqtInstallationData.steps.map((faqstep:StepItem)=><div className={scss.step}>
-				<p>
-					{faqstep.txt}
-				</p>
-				<img src={faqstep.img} alt="" />
-			</div>)
-		}
-         </div>
+          {faqtInstallationData.steps.map((faqstep: StepItem) => (
+            <div className={scss.step}>
+              <p>{faqstep.txt}</p>
+              <img src={faqstep.img} alt="" />
+            </div>
+          ))}
+        </div>
       </Panel>
     </Collapse>
   </div>
