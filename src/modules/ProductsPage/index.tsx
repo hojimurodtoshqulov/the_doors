dd940824f4f7315a1a798f135784240027b97ba9
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-05-28 22:52:28 +0500

    updated

--- src/modules/ProductsPage/index.tsx
+++ src/modules/ProductsPage/index.tsx
@@ -10,35 +10,32 @@ import { useTarjima } from "@/utils/getContent";
 import PartnersSection from "@/components/sections/PartnersSection";
 
 function ProjectsPage() {
-  const intl = useIntl();
-  const t = (id: string) => {
-    return intl?.formatMessage({ id: id });
-  };
-  const [showcase, setShowcase] = useState<any>({});
-  const getContent = useTarjima();
-  useEffect(() => {
-    axios
-      .get("https://the-doors.herokuapp.com/api/show-case/53")
-      .then((res) => {
-        setShowcase(res.data);
-      });
-  }, []);
+	const intl = useIntl();
+	const t = (id: string) => {
+		return intl?.formatMessage({ id: id });
+	};
+	const [showcase, setShowcase] = useState<any>({});
+	const getContent = useTarjima();
+	useEffect(() => {
+		axios
+			.get("https://the-doors.herokuapp.com/api/show-case/53")
+			.then((res) => {
+				setShowcase(res.data);
+			});
+	}, []);
 
-  return (
-    <div>
-      <AboutShowcase
-        image={`https://the-doors.herokuapp.com/api/files/${showcase.attachmentContentIds?.[0]}`}
-        p={getContent(showcase.descriptionRu, showcase.descriptionUz) || ""}
-      >
-        {getContent(showcase.titleRu, showcase.titleUz)}
-      </AboutShowcase>
-      <PartnersSection />
-      {/* <OurProjects /> */}
-        <ProductSection />
-
-      <Adds />
-    </div>
-  );
+	return (
+		<div>
+			<AboutShowcase
+				image={`https://the-doors.herokuapp.com/api/files/${showcase.attachmentContentIds?.[0]}`}
+				p={getContent(showcase.descriptionRu, showcase.descriptionUz) || ""}
+			>
+				{getContent(showcase.titleRu, showcase.titleUz)}
+			</AboutShowcase>
+			<ProductSection />
+			<Adds />
+		</div>
+	);
 }
 
 export default ProjectsPage;
