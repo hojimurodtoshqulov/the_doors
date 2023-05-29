69bd09733f96fc6c7366abc5b4acae8cb5f526e4
diyorbek-yusupov <dyuzb2004@gmail.com>
2023-05-30 00:10:23 +0500

    testing seo

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,30 @@
 import AboutPage from "@/modules/AboutPage";
 import ContactPage from "@/modules/ContactPage";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
-  return <ContactPage />;
+  return (
+    <>
+      <NextSeo
+        title="THEDOORS: Контакты"
+        description="thedoors: Городской офис в Ташкенте
+        Адрес: Узбекистан, г. Ташкент, ул. Фаргона Йули, 11А
+        E-mail: asduz@mail.ru
+        Сайт: www.thedoors.uz
+        Режим работы: понедельник — пятница с 9:00 до 17:00, без перерыва"
+        openGraph={{
+          title: "THEDOORS: Контакты",
+          description:
+            "thedoors: Городской офис в Ташкенте Адрес: Узбекистан, г. Ташкент, ул. Фаргона Йули, 11А E-mail: asduz@mail.ru Сайт: www.thedoors.uz Режим работы: понедельник — пятница с 9:00 до 17:00, без перерыва",
+          images: [
+            {
+              url: "	https://the-doors.herokuapp.com/api/files/13",
+              alt: "The doors: contact seo image",
+            },
+          ],
+        }}
+      />
+      <ContactPage />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { NextSeo } from "next-seo";
 import HomePage from "@/modules/HomePage";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <>
+      <NextSeo
+        title="THEDOORS: АВТОМАТИЧЕСКИЕ ВОРОТА"
+        description="thedoors: Секционные гаражные ворота - это идеальный выбор в пользу качества, надёжности, долговечности и стиля!"
+        openGraph={{
+          title: "THEDOORS: АВТОМАТИЧЕСКИЕ ВОРОТА",
+          description:
+            "thedoors: Секционные гаражные ворота - это идеальный выбор в пользу качества, надёжности, долговечности и стиля!",
+          images: [
+            {
+              url: "	https://the-doors.herokuapp.com/api/files/74",
+              alt: "The doors: seo image",
+            },
+          ],
+        }}
+      />
+      <HomePage />
+    </>
+  );
 }
